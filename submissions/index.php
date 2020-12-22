2f2866ff483a9f59348d2054ec3ceec998cfb34b
Vk <v@yopmail.com>
2020-12-22 20:53:31 +0530

    added gallaery code

--- submissions/index.php
+++ submissions/index.php
@@ -1,4 +1,26 @@
 <?php
-	header('Location: /');
-	exit;
+	if(!isset($_GET['g'])){
+		header('Location: /');
+		exit;
+	}
+	
+	define('UPLOAD_DIR', 'submissions/');
+    define('BASE_URL', 'https://laugfs-super.prvw.ceynet.asia/');
+	$files = array_diff(scandir('../'.UPLOAD_DIR), array('.', '..','index.php'));
+	rsort($files);
+	foreach ($files as $key => $value) { 
+		$artist = explode('-', $value); ?>
+		<div style="float: left; border: 1px solid;">
+			<a href="<?php echo BASE_URL.'?uid='.substr($value,0,-4); ?>">
+				<span>
+					<img width="250" src="<?php echo $value; ?>" />
+				</span>
+				<div style="text-align: center; background-color: yellow; height: 20px;">
+					<pre><?php echo $artist[2].' ('.substr($artist[3],0,-4).')'; ?></pre>
+				</div>
+			</a>
+		</div>
+		<?php
+	} 
+	
 ?>
